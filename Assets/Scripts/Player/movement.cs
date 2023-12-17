f3b55a2c4d1066cd575c2010e029454798a31ff7
edoelas <miego.cvcv@gmail.com>
2023-12-17 12:50:59 +0100

    RTDESKInputManager not working

--- Assets/Scripts/Player/movement.cs
+++ Assets/Scripts/Player/movement.cs
@@ -1,16 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Windows.WebCam;
 
+[RequireComponent(typeof(RTDESKEntity))]
 public class movement : MonoBehaviour
 {
+    RTDESKEngine Engine;  
+    
     public Vector2 dir = new Vector2(0, 0);
     public float speed;
+    
+    private void Awake()
+    {
+        //Asignar el "listener" al componente normalizado que contienen todos los objetos que pueden recibir mensajes
+        GetComponent<RTDESKEntity>().MailBox = ReceiveMessage;
+    }
     
     // Start is called before the first frame update
     void Start()
     {
+        GameObject      engine = GameObject.Find(RTDESKEngine.Name);
+
+        Engine = engine.GetComponent<RTDESKEngine>();
+        RTDESKInputManager IM = engine.GetComponent<RTDESKInputManager>();
+
+        //Register keys that we want to be signaled in case the user press them
+        IM.RegisterKeyCode(ReceiveMessage, KeyCode.W);
+        IM.RegisterKeyCode(ReceiveMessage, KeyCode.A);
+        IM.RegisterKeyCode(ReceiveMessage, KeyCode.S);
+        IM.RegisterKeyCode(ReceiveMessage, KeyCode.D);
     }
 
     // Update is called once per frame
@@ -22,5 +38,10 @@ public class movement : MonoBehaviour
         
         // move player
         transform.Translate(dir.normalized * speed * Time.deltaTime);
+    }
+    
+    void ReceiveMessage(MsgContent Msg)
+    {
+        Debug.Log("patata");
     }
 }

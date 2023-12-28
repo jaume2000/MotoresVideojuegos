5acc846b71bc6dcf36b432b3c4443d5363d2a343
jaume2000 <jaumeig95@gmail.com>
2023-12-28 19:25:35 +0100

    Refactorizado BulletPattern y Circular Pattern para amyor abstracción en los generadores de bullets

--- Assets/Scripts/Bullets/Generator/BulletPattern.cs
+++ Assets/Scripts/Bullets/Generator/BulletPattern.cs
@@ -5,4 +5,76 @@ using UnityEngine;
 public abstract class BulletPattern : MonoBehaviour, BulletGenerator {
 
     public GameObject[] bullet_prefabs;
+
+    protected float awaiting_time = 1f;
+    protected bool activated = true;
+
+    protected RTDESKEngine engine;
+    protected RTDESKEntity entity;
+    public void activate(){activated = true;}
+    public void deactivate(){activated = false;}
+
+    private void Awake()
+    {
+        //Asignar el "listener" al componente normalizado que contienen todos los objetos que pueden recibir mensajes
+        GetComponent<RTDESKEntity>().MailBox = MailBox;
+    }
+
+    void Start()
+    {
+        entity = GetComponent<RTDESKEntity>();
+        engine = entity.RTDESKEngineScript;
+
+        start();
+
+        ObjectMsg Msg  = (ObjectMsg)engine.PopMsg((int)UserMsgTypes.Object);
+        Msg.o = null;
+        engine.SendMsg(Msg, gameObject, MailBox, engine.ms2Ticks(0));
+        
+    }
+
+    protected void MailBox (MsgContent Msg)
+    {
+        switch (Msg.Type)
+        {
+            //Queremos generar N bullets.
+            case (int)UserMsgTypes.Object:
+                if(!player.gameover){
+                    generator_loop();
+                }
+                break;
+
+            //Faltan los casos Action (start) y Action (end) que nos permitan parar y reanudar el ciclo recursivo desde Generator Manager
+
+            case (int)UserMsgTypes.Action:
+                if(!player.gameover){
+                    //Hacer cosas
+
+                    //Switch case de Action start, end (etc.)
+
+                }
+                break;
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    protected void generator_loop(){
+
+        generate();
+
+        ObjectMsg Msg  = (ObjectMsg)engine.PopMsg((int)UserMsgTypes.Object);
+        Msg.o = null;
+        engine.SendMsg(Msg, gameObject, MailBox, engine.ms2Ticks(awaiting_time*1000));
+    }
+
+    protected abstract void generate();
+    protected abstract void start();
+
+    
 }
+
+
--- Assets/Scripts/Bullets/Generator/CircularPattern.cs
+++ Assets/Scripts/Bullets/Generator/CircularPattern.cs
@@ -7,37 +7,18 @@ using System;
 public class CircularPattern : BulletPattern
 {
 
-    public float awaiting_time= 3;
+    public float generate_time= 3;
     public float phase_shift = 0.1f;
     private float total_phase = 0;
     public float bullet_velocity = 5;
     public int bullet_count = 5;
     public int survival_time = 5;
-
-    protected float time_transcured = 0;
-
-    private RTDESKEngine engine;
-    private RTDESKEntity entity;
-
-    private void Awake()
-    {
-        //Asignar el "listener" al componente normalizado que contienen todos los objetos que pueden recibir mensajes
-        GetComponent<RTDESKEntity>().MailBox = MailBox;
-    }
     
-    void Start()
-    {
-        entity = GetComponent<RTDESKEntity>();
-        engine = entity.RTDESKEngineScript;
-
-
-        ObjectMsg Msg  = (ObjectMsg)engine.PopMsg((int)UserMsgTypes.Object);
-        Msg.o = null;
-        engine.SendMsg(Msg, gameObject, MailBox, engine.ms2Ticks(awaiting_time*500));
-        
+    protected override void start(){
+        awaiting_time = generate_time;
     }
 
-    public void genertate(){
+    protected override void generate(){
 
 
         for(int i=0; i< bullet_count; i++){
@@ -51,31 +32,5 @@ public class CircularPattern : BulletPattern
         total_phase+=phase_shift;
         total_phase = total_phase % 360;
 
-    }
-
-    void MailBox (MsgContent Msg)
-    {
-        switch (Msg.Type)
-        {
-            //Queremos generar N bullets.
-            case (int)UserMsgTypes.Object:
-                if(!player.gameover){
-                    loop();
-                }
-                break;
-
-            default:
-                break;
-        }
-
-    }
-
-    void loop(){
-
-        genertate();
-
-        ObjectMsg Msg  = (ObjectMsg)engine.PopMsg((int)UserMsgTypes.Object);
-        Msg.o = null;
-        engine.SendMsg(Msg, gameObject, MailBox, engine.ms2Ticks(awaiting_time*1000));
     }
 }
